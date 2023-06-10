69fa3a9e499b5f00e35a2fcd9079ca34c6058636
isadfrn <isadfrn@gmail.com>
2023-06-09 23:29:03 -0300

    feat: Add fps counter

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -9,7 +9,10 @@ import java.awt.*;
 public class GamePanel extends JPanel {
 
     private MouseInputs mouseInputs;
-    private int xDelta = 100, yDelta = 100;
+    private float xDelta = 100, yDelta = 100;
+    private float xDir = 0.003f, yDir = 0.003f;
+    private int frames = 0;
+    private long lastCheck = 0;
     public GamePanel() {
         mouseInputs = new MouseInputs(this);
         addKeyListener(new KeyboardInputs(this));
@@ -19,23 +22,47 @@ public class GamePanel extends JPanel {
 
     public void changeXDelta(int value) {
         this.xDelta += value;
-        repaint();
     }
 
     public void changeYDelta(int value) {
         this.yDelta += value;
-        repaint();
     }
 
     public void setRectPosition(int x, int y) {
         this.xDelta = x;
         this.yDelta = y;
-        repaint();
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        g.fillRect(xDelta, yDelta, 200, 50);
+        updateReactangle();
+
+        g.setColor(new Color(150, 20, 98));
+
+        g.fillRect((int) xDelta, (int)yDelta, 200, 50);
+
+        frames++;
+
+        if (System.currentTimeMillis() - lastCheck >= 1000) {
+            lastCheck = System.currentTimeMillis();
+            System.out.println("FPS: " + frames);
+            frames = 0;
+        }
+
+        repaint();
+    }
+
+    private void updateReactangle() {
+        xDelta += xDir;
+
+        if (xDelta > 400 || xDelta < 0) {
+            xDir *= -1;
+        }
+
+        yDelta += yDir;
+        if (yDelta > 400 || yDelta < 0) {
+            yDir *= -1;
+        }
     }
 }
